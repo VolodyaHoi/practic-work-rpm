899e5872992fd511ff7a9a25c4ecb7e2c11bd242
Vladimir Mansurov <70720123+VolodyaHoi@users.noreply.github.com>
2024-01-22 18:10:41 +0300

    upload my work

---
+++ practic work git/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using practicLib;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tb_q1_clear(object sender, MouseButtonEventArgs e)
+        {
+            tb_q1_num.Text = "";
+        }
+
+        private void btn_q1_start(object sender, RoutedEventArgs e)
+        {
+            bool isNum = int.TryParse(tb_q1_num.Text, out int num);
+            if (isNum)
+            {
+                if (num >= 10 && num <= 99)
+                {
+                    bool result = practicLib.practicLib.equalOfNumbers(num);
+                    if (result)
+                    {
+                        MessageBox.Show("Цифры данного числа одинаковы!");
+                    }
+                    else { MessageBox.Show("Цифры данного числа не одинаковы!"); }
+                }
+                else { MessageBox.Show("Число не двухзначное!"); }
+            }
+            else { MessageBox.Show("Введенное значение не корректно!"); }
+        }
+
+        private void tb_q2_a_clear(object sender, MouseButtonEventArgs e)
+        {
+            tb_q2_anum.Text = "";
+        }
+
+        private void tb_q2_b_clear(object sender, MouseButtonEventArgs e)
+        {
+            tb_q2_bnum.Text = "";
+        }
+
+        private void tb_q2_c_clear(object sender, MouseButtonEventArgs e)
+        {
+            tb_q2_cnum.Text = "";
+        }
+
+        private void btn_q2_start(object sender, RoutedEventArgs e)
+        {
+            double A, B, C;
+
+            bool isNumA = double.TryParse(tb_q2_anum.Text, out double numA);
+            bool isNumB = double.TryParse(tb_q2_bnum.Text, out double numB);
+            bool isNumC = double.TryParse(tb_q2_cnum.Text, out double numC);
+
+            if (isNumA && isNumB && isNumC)
+            {
+                practicLib.practicLib.threeNumbersPow(numA, numB, numC, out A, out B, out C);
+                MessageBox.Show($"Числа возведенные и не возведенные в квадрат: {A}, {B}, {C}");
+            }
+            else { MessageBox.Show("Введенное значение не корректно!"); }
+
+        }
+
+        private void btn_q3_add(object sender, RoutedEventArgs e)
+        {
+            bool isNum = int.TryParse(tb_q3_num.Text, out int num);
+
+            if (isNum)
+            {
+                lb_q3.Items.Add(num.ToString());
+            }
+            else { MessageBox.Show("Введенное значение не корректно!"); }
+        }
+
+        private void btn_q3_start(object sender, RoutedEventArgs e)
+        {
+            bool empty = lb_q3.Items.IsEmpty;
+            if (empty == false)
+            {
+                int[] array = new int[lb_q3.Items.Count];
+
+                for (int i = 0; i < lb_q3.Items.Count; i++)
+                {
+                    array[i] = Convert.ToInt32(lb_q3.Items[i]);
+                }
+
+                int result = practicLib.practicLib.sumOfNumbers(array);
+                MessageBox.Show($"Сумма: {result}");
+            }
+            else { MessageBox.Show("Массив пустой!"); }
+        }
+
+        private void btn_q3_clear(object sender, RoutedEventArgs e)
+        {
+            lb_q3.Items.Clear();
+        }
+
+        private void btn_q3_delete(object sender, RoutedEventArgs e)
+        {
+            lb_q3.Items.Remove(lb_q3.SelectedItem);
+        }
+
+        private void tb_q4_m_clear(object sender, MouseButtonEventArgs e)
+        {
+            tb_q4_m.Text = "";
+        }
+
+        private void tb_q4_n_clear(object sender, MouseButtonEventArgs e)
+        {
+            tb_q4_n.Text = "";
+        }
+
+        private void btn_q4_start(object sender, RoutedEventArgs e)
+        {
+            bool isNumM = int.TryParse(tb_q4_m.Text, out int M);
+            bool isNumN = int.TryParse(tb_q4_n.Text, out int N);
+            int[,] matrix = new int[M, N];
+            bool error = false;
+            for (int row = 0; row < M; row++)
+            {
+                var dataItem = dg_matrix.Items[row];
+
+                for (int col = 0; col < N; col++)
+                {
+                    var cellValue = string.Empty;
+
+                    var cellContent = dg_matrix.Columns[col].GetCellContent(dataItem);
+                    if (cellContent is TextBlock textBlock)
+                    {
+                        cellValue = textBlock.Text;
+                    }
+
+                    bool isNum = int.TryParse(cellValue, out int value);
+
+                    if (isNum)
+                    {
+                        matrix[row, col] = value;
+                    } else 
+                    { 
+                        MessageBox.Show("Одно из значений матрицы не корректно!");
+                        error = true;
+                        break;
+                    }
+                }
+            }
+            if (error == false) 
+            {
+                int res = practicLib.practicLib.columnMaxEqualPos(matrix);
+                MessageBox.Show($" Номер первого столбца с максимальным кол-вом повторений: {res + 1}");
+            }  
+        }
+
+        private void tb_q3_clear(object sender, MouseButtonEventArgs e)
+        {
+            tb_q3_num.Text = "";
+        }
+
+        private void btn_q4_create(object sender, RoutedEventArgs e)
+        {
+            bool isNumM = int.TryParse(tb_q4_m.Text, out int M);
+            bool isNumN = int.TryParse(tb_q4_n.Text, out int N);
+
+            int[,] matrix = new int[M, N];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = 0;
+                }
+            }
+
+            dg_matrix.ItemsSource = practicLib.practicLib.ToDataTable(matrix).DefaultView;
+        }
+    }
+}
---
+++ practic work git/WpfApp1/practicLib.cs
@@ -0,0 +1,172 @@
+﻿using System.Data;
+
+namespace practicLib
+{
+    public static class practicLib
+    {
+        /// <summary>
+        /// Ввести двухзначное число. Определить: одинаковы ли его цифры.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static bool equalOfNumbers(int number)
+        {
+            // Инициализация необходимых переменных
+            bool equal;
+
+            int first_number;
+            int second_number;
+
+            first_number = number / 10; // Получение первого числа
+            second_number = number % 10; // Получение второго числа
+
+
+            if (first_number == second_number) // Если первое и второе число совпадают, вернуть true
+            {                                   // В противном случае вернуть false
+                equal = true;
+            } else { equal = false; }
+
+            return equal;
+        }
+
+        /// <summary>
+        /// Ввести три целых числа. Возвести в квадрат те из них, значения которых неотрицательны.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="a_pow"></param>
+        /// <param name="b_pow"></param>
+        /// <param name="c_pow"></param>
+        public static void threeNumbersPow(double a, double b, double c, out double a_pow, out double b_pow, out double c_pow)
+        {
+            // Инициализация необходимых переменных
+            a_pow = 0;
+            b_pow = 0;
+            c_pow = 0;
+
+            if (a > 0) // В случае если число больше 0, возвести его в квадрат
+            {
+                a_pow = Math.Pow(a, 2);
+            } else { a_pow = a; }
+            if (b > 0)
+            {
+                b_pow = Math.Pow(b, 2);
+            } else { b_pow = b; }
+            if (c > 0)
+            {
+                c_pow = Math.Pow(c, 2);
+            } else { c_pow = c;  }
+
+        }
+
+        /// <summary>
+        /// Составьте программу вычисления в массиве суммы всех чисел, кратных 3.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int sumOfNumbers(int[] array)
+        {
+            // Инициализация необходимых переменных
+            int sum = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++) // Перебор всех элементов массива
+            {
+                if (array[i] % 3 == 0) // В случае если элемент кратен 3, сложить его с текущим значением суммы
+                {
+                    sum += array[i];
+                }
+            }
+
+            return sum;
+        }
+
+        /// <summary>
+        /// Дана целочисленная матрица размера M * N. Найти номер первого из её столбцов, содержащих максимальное количество одинаковых элементов.
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public static int columnMaxEqualPos(int[,] matrix)
+        {
+            // Инициализация необходимых переменных
+            int m = matrix.GetLength(0), n = matrix.GetLength(1), max_value, dup_count = 0, cur_column = -1;
+
+            int[] array = new int[n];
+
+            for (int j = 0; j < n; j++) // Перебор столбцов матрицы
+            {
+                for (int i = 0; i < m - 1; i++)
+                {
+                    if (matrix[i, j] == matrix[i + 1, j]) // Если найдено повторение, увеличить количество повторений
+                    {
+                        dup_count += 1;
+                    }
+                }
+                array[j] = dup_count; // Добавить количество повторений в отдельный массив
+                dup_count = 0; // Обнуление счетчика повторений
+            }
+
+            max_value = array[0];
+
+
+            for (int i = 0; i < n; i++) // Поиск самого большого значения
+            {
+                if (max_value < array[i])
+                {
+                    max_value = array[i];
+                }
+            }
+
+            for (int i = 0; i < n; i++) // Поиск первого столбца с самым большим значением
+            {
+                if (array[i] == max_value) // Когда было найдено, закончить выполнение цикла
+                {
+                    cur_column = i;
+                    break;
+                }
+            }
+
+            return cur_column; // Возвращение индекса первого столбца с самым большим значением
+        }
+
+        public static DataTable ToDataTable<T>(this T[] arr)
+        {
+            var res = new DataTable();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                res.Columns.Add("col" + (i + 1), typeof(T));
+            }
+            var row = res.NewRow();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                row[i] = arr[i];
+            }
+            res.Rows.Add(row);
+            return res;
+        }
+        //Метод для двухмерного массива
+        public static DataTable ToDataTable<T>(this T[,] matrix)
+        {
+            var res = new DataTable();
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                res.Columns.Add("col" + (i + 1), typeof(T));
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var row = res.NewRow();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    row[j] = matrix[i, j];
+                }
+
+                res.Rows.Add(row);
+            }
+
+            return res;
+        }
+
+    }
+}
